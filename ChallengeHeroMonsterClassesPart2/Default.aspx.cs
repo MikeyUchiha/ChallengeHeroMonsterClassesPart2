da205aaddecdd43f0b47780d5ca673fc4a04d266
MikeyUchiha <mikey@starfantasygames.com>
2015-07-05 21:50:29 -0700

    Initial Base From Previous Challenge
    Added the code from the Part 1 of HeroMonsterClasses.

--- ChallengeHeroMonsterClassesPart2/Default.aspx.cs
+++ ChallengeHeroMonsterClassesPart2/Default.aspx.cs
@@ -11,7 +11,49 @@ namespace ChallengeHeroMonsterClassesPart2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.Health = 100;
+            hero.DamageMaximum = 40;
+            hero.AttackBonus = true;
 
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.Health = 100;
+            monster.DamageMaximum = 25;
+            monster.AttackBonus = false;
+
+            monster.Defend(hero.Attack());
+            hero.Defend(monster.Attack());
+
+            DisplayStats(hero);
+            DisplayStats(monster);
+        }
+
+        private void DisplayStats(Character character)
+        {
+            resultLabel.Text += String.Format("Name: {0}, Health: {1}, Damage Maximum: {2}, Attack Bonus: {3}</br>",
+                character.Name, character.Health, character.DamageMaximum, character.AttackBonus);
+        }
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public int Attack()
+        {
+            Random random = new Random();
+            int result = random.Next(DamageMaximum);
+            return result;
+        }
+
+        public void Defend(int damage)
+        {
+            Health -= damage;
         }
     }
 }
